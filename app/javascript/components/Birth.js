45f4b79f7a0551d6e07ad162f4c5603ccbd9262a
Goi <kakedashiit@gmail.com>
2019-07-13 17:35:55 +0900

    add select option is last year logic

--- app/javascript/components/Birth.js
+++ app/javascript/components/Birth.js
@@ -3,7 +3,14 @@ import PropTypes from "prop-types"
 import Select from 'react-select'
 
 const yearItems = [];
-for (let i = 1950; i <= 2000; i++) {
+const adultCheckYear = () => {
+  const dt = new Date();
+  const nowYear = dt.getFullYear();
+  const lastYear = Number(nowYear) - 18;
+  return lastYear;
+}
+
+for (let i = 1950; i <= adultCheckYear(); i++) {
   yearItems.push(
     { value: i, label: i},
   )
@@ -22,19 +29,15 @@ for (let i = 1; i <= 31; i++) {
 }
 
 
-const Year = () => (
-  <Select id={"yearSelect"} name={"private_users[birth1]"} options={yearItems} placeholder={"年"} value={this.state.value}/>
-)
-const Month = () => (
-  <Select name={"private_users[birth2]"} options={monthItems} placeholder={"月"} />
-)
-const Day = () => (
-  <Select name={"private_users[birth3]"} options={dayItems} placeholder={"日"}/>
-)
-
-const getMonthDays = () => {  
-  console.log("Test");
-};
+// const Year = () => (
+//   <Select id={"yearSelect"} name={"private_users[birth1]"} options={yearItems} placeholder={"年"} value={this.state.value}/>
+// )
+// const Month = () => (
+//   <Select name={"private_users[birth2]"} options={monthItems} placeholder={"月"} />
+// )
+// const Day = () => (
+//   <Select name={"private_users[birth3]"} options={dayItems} placeholder={"日"}/>
+// )
 
 let textInput = React.createRef();
 
@@ -44,6 +47,8 @@ class Birth extends React.Component {
     super(props);
     this.state = {
       year: '',
+      month: '',
+      dayset: '',
       day: dayItems
     }
     this.handleChange = this.handleChange.bind(this)
@@ -54,6 +59,7 @@ class Birth extends React.Component {
   }
 
   getMonthDays = (e) => {
+    this.setState({month: e.value});
     const year = this.state.year;
     const month_tmp = e.value;
 
@@ -65,7 +71,6 @@ class Birth extends React.Component {
     }
 
     const lastDay = new Date(year, month + 1, 0).getDate();
-    console.log(lastDay);
     var dayItems = [];
     for (let i = 1; i <= Number(lastDay); i++) {
       dayItems.push(
@@ -74,6 +79,19 @@ class Birth extends React.Component {
     }
     this.setState({day: dayItems});
   };
+
+  checkAdult = (e) => {
+    const year = String(this.state.year);
+    let month = String(this.state.month);
+    if (month.length === 1) {
+      month = '0' + month;
+    }
+    let day = String(e.value);
+    if (day.length === 1) {
+      day = '0' + day;
+    }
+    console.log( year + month + day);
+  }
 
   render () {
     return (
@@ -99,6 +117,7 @@ class Birth extends React.Component {
               name={"private_users[birth3]"}
               options={this.state.day}
               placeholder={"日"}
+              onChange={this.checkAdult}
             />
         </div>
       </React.Fragment>
